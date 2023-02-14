608b7d0a7649db690ed413fd06a529c70b07d406
sean butler <butlersean@gmail.com>
2023-02-14 20:34:29 +0000

    First Push of First Prompt

---
+++ Prompt02/main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by sean on 14/02/23.
+//
+// https://chat.openai.com/chat using prompt "generate a c++ program for a simple video game"
+//
+
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
